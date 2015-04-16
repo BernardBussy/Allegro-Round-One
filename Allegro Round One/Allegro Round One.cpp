5275ea64342bf207a2d8bc1e90476158fb055a45
BernardBussy <213533986@stu.ukzn.ac.za>
2015-04-16 22:49:11 +0200

    text working

--- Allegro Round One/Allegro Round One.cpp
+++ Allegro Round One/Allegro Round One.cpp
@@ -2,7 +2,8 @@
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
 #include <allegro5\allegro_native_dialog.h>
-
+#include <iostream>
+using namespace std;
 int main(void)
 {
 	ALLEGRO_DISPLAY *display = NULL;
@@ -30,9 +31,15 @@ int main(void)
 	al_draw_text(fontArial24, al_map_rgb(255, 0, 255), 50, 50, 0, "Hellow world, this is 24 Point");
 	al_draw_text(fontarial18, al_map_rgb(244, 123, 44), 640 / 2, 480 / 2, ALLEGRO_ALIGN_CENTER, "This text is centred");
 	al_draw_text(fontcalibri36, al_map_rgb(23, 244, 55), 620, 350, ALLEGRO_ALIGN_RIGHT, "This is right aligned and 36 Point");
+	
+	
+	int screen_w = al_get_display_width(display);
+	int screen_h = al_get_display_height(display);
+	cout << screen_h << " and " << screen_w << endl;
+
+	al_draw_textf(fontarial18, al_map_rgb(255, 255, 255), screen_w / 2, 400, ALLEGRO_ALIGN_CENTRE, "TEXT with variable output (textf): Screen width and height = %i / %i", screen_w, screen_h);
 	al_flip_display();
 	al_rest(5);
-	
 	al_destroy_display(display);
 	return 0;
 }
