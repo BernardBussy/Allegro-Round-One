a26aaf11880fda87d03217492b3d1e3143b51b42
BernardBussy <213533986@stu.ukzn.ac.za>
2015-04-16 22:37:55 +0200

    added text draw

--- Allegro Round One/Allegro Round One.cpp
+++ Allegro Round One/Allegro Round One.cpp
@@ -1,4 +1,6 @@
 #include <allegro5\allegro.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
 #include <allegro5\allegro_native_dialog.h>
 
 int main(void)
@@ -17,6 +19,20 @@ int main(void)
 		return -1;
 	}
 
+	al_init_font_addon();
+	al_init_ttf_addon();
+
+	ALLEGRO_FONT *fontArial24 = al_load_font("arial.ttf", 24, 0);
+	ALLEGRO_FONT *fontcalibri36 = al_load_font("calibri.ttf", 36, 0);
+	ALLEGRO_FONT *fontarial18 = al_load_font("arial.ttf", 18, 0);
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_draw_text(fontArial24, al_map_rgb(255, 0, 255), 50, 50, 0, "Hellow world, this is 24 Point");
+	al_draw_text(fontarial18, al_map_rgb(244, 123, 44), 640 / 2, 480 / 2, ALLEGRO_ALIGN_CENTER, "This text is centred");
+	al_draw_text(fontcalibri36, al_map_rgb(23, 244, 55), 620, 350, ALLEGRO_ALIGN_RIGHT, "This is right aligned and 36 Point");
+	al_flip_display();
+	al_rest(5);
+	
 	al_destroy_display(display);
 	return 0;
 }
